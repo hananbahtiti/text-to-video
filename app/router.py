9af7d4a004fd38ea56639656ba5f7b724a3b7fdd
Hanan Bahtiti <55956102+hananbahtiti@users.noreply.github.com>
2025-06-21 17:43:51 +0300

    Update router.py

--- app/router.py
+++ app/router.py
@@ -4,16 +4,16 @@ from tasks import task_model_flux_pro, task_model_flux_pro_ultra, task_model_flu
 def run_model_task(**kwargs):
     model_name = kwargs.get("model_name")
     
-    if model_name == "fal-ai/flux-pro/v1.1":
-        task_model_flux_pro.generate_image(**kwargs)
-    elif model_name == "fal-ai/flux-pro/v1.1-ultra":
-        task_model_flux_pro_ultra.generate_image(**kwargs)
-    elif model_name == "fal-ai/flux/dev":
-        task_model_flux_dev.generate_image(**kwargs)
-    elif model_name == "fal-ai/ideogram/v2":
-        task_model_ideogram_v2.generate_image(**kwargs)
-    elif model_name == "fal-ai/flux/schnell":
-        task_model_flux_schnell.generate_image(**kwargs)
+    if model_name == "fal-ai/luma-dream-machine/ray-2":
+        task_model_luma_dream_machine_ray_2.generate_image(**kwargs)
+    elif model_name == "fal-ai/luma-dream-machine":
+        task_model_luma_dream_machine.generate_image(**kwargs)
+    elif model_name == "fal-ai/kling-video/v1.6/standard/text-to-video":
+        task_model_kling_video_v1_6.generate_image(**kwargs)
+    elif model_name == "fal-ai/cogvideox-5b":
+        task_model_cogvideox_https://fal.ai/models/fal-ai/hunyuan-video5b.generate_image(**kwargs)
+    elif model_name == "fal-ai/hunyuan-video":
+        task_model_hunyuan_video.generate_image(**kwargs)
     else:
         logging.error(f"Unsupported model: {model_name}")
         raise ValueError(f"Unsupported model: {model_name}")
