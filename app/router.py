f80cb8b092848681fb9c3d8e441eda1d0dab9101
Hanan Bahtiti <55956102+hananbahtiti@users.noreply.github.com>
2025-06-21 17:45:18 +0300

    Update router.py

--- app/router.py
+++ app/router.py
@@ -1,5 +1,5 @@
 import logging
-from tasks import task_model_flux_pro, task_model_flux_pro_ultra, task_model_flux_dev, task_model_ideogram_v2, task_model_flux_schnell  # Add all models here
+from tasks import task_model_luma_dream_machine_ray_2, task_model_luma_dream_machine, task_model_kling_video_v1_6, task_model_cogvideox_5b, task_model_hunyuan_video # Add all models here
 
 def run_model_task(**kwargs):
     model_name = kwargs.get("model_name")
@@ -11,7 +11,7 @@ def run_model_task(**kwargs):
     elif model_name == "fal-ai/kling-video/v1.6/standard/text-to-video":
         task_model_kling_video_v1_6.generate_image(**kwargs)
     elif model_name == "fal-ai/cogvideox-5b":
-        task_model_cogvideox_https://fal.ai/models/fal-ai/hunyuan-video5b.generate_image(**kwargs)
+        task_model_cogvideox_5b.generate_image(**kwargs)
     elif model_name == "fal-ai/hunyuan-video":
         task_model_hunyuan_video.generate_image(**kwargs)
     else:
